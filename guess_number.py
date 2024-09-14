e95b1d3efad6480dca537f925b2652cb5741c258
Sami Kedir <samikedir9502@gmail.com>
2024-09-13 21:14:04 -0500

    Modified code to limit number of tries the user has

--- guess_number.py
+++ guess_number.py
@@ -1,7 +1,7 @@
 number = 10
 print("I'm thinking of a number...")
-
-while True:
+tries = 3
+while tries > 0:
     guess = input("What number am I thinking of? ")
 
     if guess.isdigit() and int(guess) == number:
@@ -11,6 +11,12 @@ while True:
         print(f"The right number was {number}! Maybe next time!")
         break    
     else:
-        print("Sorry! That's not quite right, try again! (Enter 'q' if you'd like to quit)")
+        tries -= 1
+        if tries > 1:
+            print(f"Sorry! That's not quite right, {tries} more tries! (Enter 'q' if you'd like to quit)")
+        elif tries == 1:
+            print(f"Sorry! That's not quite right, {tries} more try! (Enter 'q' if you'd like to quit)")
+        else:
+            print(f"Out of tries! The right number was {number}.")
 
 
