bc339116e1fc9f6f2e0f77d3230ddbfb1f2cf0ef
Sami Kedir <samikedir9502@gmail.com>
2024-09-13 20:46:07 -0500

    Modified starter code for guess_number.py, now gives users multiple tries as well as the option to quit

--- guess_number.py
+++ guess_number.py
@@ -1,10 +1,16 @@
 number = 10
-
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
 
-if guess == number:
-    print("Congratulations! You guessed the right number.")
-else:
-    print(f"Sorry! The number was {number}.")
-    
+while True:
+    guess = input("What number am I thinking of? ")
+
+    if guess.isdigit() and int(guess) == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    if guess == "q":
+        print(f"The right number was {number}! Maybe next time!")
+        break    
+    else:
+        print("Sorry! That's not quite right, try again! (Enter 'q' if you'd like to quit)")
+
+
